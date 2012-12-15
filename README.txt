6c2eb55c12da72dc0fa351bfbd8cd38bd4387121
Wolfgang Schnerring <wosc@wosc.de>
2012-12-15 07:56:48 +0100

    Create skeleton

---
+++ README.txt
@@ -0,0 +1,7 @@
+=======================================
+The gocept.httpserverlayer distribution
+=======================================
+
+HTTP server integration for testing
+
+This package is compatible with Python version 2.7.
---
+++ setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+import glob
+import os.path
+
+
+def project_path(*names):
+    return os.path.join(os.path.dirname(__file__), *names)
+
+
+setup(
+    name='gocept.httpserverlayer',
+    version='1.0.dev0',
+
+    install_requires=[
+        'distribute',
+        'plone.testing',
+    ],
+
+    extras_require={
+        'test': [
+        ],
+    },
+
+    entry_points={
+    },
+
+    author='gocept <mail@gocept.com>',
+    author_email='mail@gocept.com',
+    license='ZPL 2.1',
+    url='https://projects.gocept.com/projects/gocept-httpserverlayer/',
+
+    keywords='',
+    classifiers="""\
+License :: OSI Approved :: Zope Public License
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 2 :: Only
+"""[:-1].split('\n'),
+    description='HTTP server integration for testing',
+    long_description='\n\n'.join(open(project_path(name)).read() for name in (
+        'README.txt',
+        'HACKING.txt',
+        'CHANGES.txt',
+    )),
+
+    namespace_packages=['gocept'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    data_files=[('', glob.glob(project_path('*.txt')))],
+    zip_safe=False,
+)
