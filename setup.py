784ac80acf86a667272b1eb3b712b4bd51a7d282
Michael Howitz <mh@gocept.com>
2017-04-20 08:19:39 +0200

    Remove no longer needed version restrictions.

--- setup.py
+++ setup.py
@@ -20,8 +20,6 @@ setup(
             'plone.testing[z2] != 5.1',
         ],
         'plonetestingz4': [
-            'ZODB < 5.0',
-            'transaction < 2.0',
             'ZServer >= 4.0a1',
             'plone.testing[z2] >= 5.1.1',
         ],
