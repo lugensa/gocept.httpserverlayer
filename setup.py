ddc75c4c0e8f3fb9aba69e6f6703f66dae9ba754
Daniel Havlik <dh@gocept.com>
2019-01-15 11:04:30 +0100

    Back to development: 3.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.httpserverlayer',
-    version='3.1',
+    version='3.2.dev0',
 
     install_requires=[
         'setuptools',
