c9ecac052cee0933ba29f2e39fbae6d8630e5ece
Michael Howitz <mh@gocept.com>
2019-04-02 16:38:59 +0200

    Fix usage of pending deprecated `Thread.isAlive()`.

--- src/gocept/httpserverlayer/wsgi/tests.py
+++ src/gocept/httpserverlayer/wsgi/tests.py
@@ -43,6 +43,6 @@ class WSGITest(unittest.TestCase):
     layer = EXAMPLE_LAYER
 
     def test_http_works(self):
-        self.assertTrue(self.layer['httpd_thread'].isAlive)
+        self.assertTrue(self.layer['httpd_thread'].is_alive())
         r = urlopen('http://%s/' % self.layer['http_address'])
         self.assertIn(b'Hello world', r.read())
